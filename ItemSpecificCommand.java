a9e35d925c64df9843f6482a8f3a4d4d7887a7d6
BKertchy <niellb@gmail.com>
2016-11-09 10:19:56 -0500

    Update ItemSpecificCommand.java

--- ItemSpecificCommand.java
+++ ItemSpecificCommand.java
@@ -9,6 +9,11 @@ class ItemSpecificCommand extends Command {
         this.noun = noun;
     }
 
+    /*If an action committed by the adventurer returns a message that is equivalent 
+      to one of the names of the events that can occur, then this execute method
+      will return the string produced from the specific item event that was called
+      on the item object. The item events are detailed in the Item class.
+    */
     public String execute() {
         
         Item itemReferredTo = null;
