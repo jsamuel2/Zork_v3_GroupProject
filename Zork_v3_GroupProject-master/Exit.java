ca6bc3150c2e9d64903f485cd9729224ea670906
Daniel <danielzamojda@gmail.com>
2016-11-09 11:19:40 -0500

    Add files via upload

---
+++ Zork_v3_GroupProject-master/Exit.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Exit {
+
+    class NoExitException extends Exception {}
+
+    private String dir;
+    private Room src, dest;
+
+    Exit(String dir, Room src, Room dest) {
+        init();
+        this.dir = dir;
+        this.src = src;
+        this.dest = dest;
+        src.addExit(this);
+    }
+
+    /** Given a Scanner object positioned at the beginning of an "exit" file
+        entry, read and return an Exit object representing it. 
+        @param d The dungeon that contains this exit (so that Room objects 
+        may be obtained.)
+        @throws NoExitException The reader object is not positioned at the
+        start of an exit entry. A side effect of this is the reader's cursor
+        is now positioned one line past where it was.
+        @throws IllegalDungeonFormatException A structural problem with the
+        dungeon file itself, detected when trying to read this room.
+     */
+    
+    /*An Exit shall now take a boolean when reading in the files that defines
+    * whether or not the exit can be accessed (is it locked or not?).
+    * For now, a locked exit will be determined to be unlocked by an item being 
+    * in the adventurer's inventory.
+    * Upon being given the command to access an exit, IFF the exit is locked, will
+    * a the Exit class call a method that returns a String saying either 1. the 
+    * exit is locked (not wanting to disclose the item) or 2. adventurer does not have 
+    * required item to access the exit, or 3. it's unlocked and the adventurer
+    * can pass.
+    *@param HashTable<Item,String> isLocked; Item in order to unlock. String that is used
+    *when exit is unlocked.
+    *@param Boolean isLocked
+    *
+    *@author Jonathan Samuelsen
+    *@author Daniel Zamojda
+    *@author Brendon Kertcher
+    */
+    Exit(Scanner s, Dungeon d, Boolean isLocked) throws NoExitException,
+        Dungeon.IllegalDungeonFormatException {
+
+        init();
+        String srcTitle = s.nextLine();
+        if (srcTitle.equals(Dungeon.TOP_LEVEL_DELIM)) {
+            throw new NoExitException();
+        }
+        src = d.getRoom(srcTitle);
+        dir = s.nextLine();
+        dest = d.getRoom(s.nextLine());
+        
+        // I'm an Exit object. Great. Add me as an exit to my source Room too,
+        // though.
+        src.addExit(this);
+
+        // throw away delimiter
+        if (!s.nextLine().equals(Dungeon.SECOND_LEVEL_DELIM)) {
+            throw new Dungeon.IllegalDungeonFormatException("No '" +
+                Dungeon.SECOND_LEVEL_DELIM + "' after exit.");
+        }
+    }
+
+    // Common object initialization tasks.
+    private void init() {
+    }
+
+    String describe() {
+        return "You can go " + dir + " to " + dest.getTitle() + ".";
+    }
+
+    String getDir() { return dir; }
+    Room getSrc() { return src; }
+    Room getDest() { return dest; }
+    
+    /*This method returns a String that was detailed in the constructor commentation.
+    * It gets the item from the 'locked' HashTable and checks the GameState.instance()
+    * to see if the item is in the adventurers inventory. If so, then it continues on,
+    * if not, it returns a String saying the Adventurer may not pass. It also udpates
+    * the exit's boolean status.
+    */
+    String isLocked(){}
+}
---
+++ Zork_v3_GroupProject-master/Room.java
@@ -0,0 +1,229 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Room {
+
+    class NoRoomException extends Exception {}
+
+    static String CONTENTS_STARTER = "Contents: ";
+
+    private String title;
+    private String desc;
+    private boolean beenHere;
+    private ArrayList<Item> contents;
+    private ArrayList<Exit> exits;
+
+    /**
+    *An arraylist of the NPCs that are in the room
+    *@author Daniel Zamojda
+    */
+    private ArrayList<NPC> npcs;
+    
+    /**
+    *A boolean that is toggled on if the room needs a light source to get its contents, 
+    *or off if the adventurer is carrying a light source
+    *@author Daniel Zamojda
+    */
+    private boolean isDark;
+    
+    Room(String title) {
+        init();
+        this.title = title;
+    }
+
+    Room(Scanner s, Dungeon d) throws NoRoomException,
+        Dungeon.IllegalDungeonFormatException {
+
+        this(s, d, true,false);
+    }
+
+    /** Given a Scanner object positioned at the beginning of a "room" file
+        entry, read and return a Room object representing it. 
+        @param d The containing {@link edu.umw.stephen.bork.Dungeon} object, 
+        necessary to retrieve {@link edu.umw.stephen.bork.Item} objects.
+        @param initState should items listed for this room be added to it?
+        @throws NoRoomException The reader object is not positioned at the
+        start of a room entry. A side effect of this is the reader's cursor
+        is now positioned one line past where it was.
+        @param isDark boolean that tells if it requires a lightsource or not
+        @throws IllegalDungeonFormatException A structural problem with the
+        dungeon file itself, detected when trying to read this room.
+     */
+    Room(Scanner s, Dungeon d, boolean initState, boolean isDark) throws NoRoomException,
+        Dungeon.IllegalDungeonFormatException {
+
+        init();
+        title = s.nextLine();
+        desc = "";
+        if (title.equals(Dungeon.TOP_LEVEL_DELIM)) {
+            throw new NoRoomException();
+        }
+        
+        String lineOfDesc = s.nextLine();
+        while (!lineOfDesc.equals(Dungeon.SECOND_LEVEL_DELIM) &&
+               !lineOfDesc.equals(Dungeon.TOP_LEVEL_DELIM)) {
+
+            if (lineOfDesc.startsWith(CONTENTS_STARTER)) {
+                String itemsList = lineOfDesc.substring(CONTENTS_STARTER.length());
+                String[] itemNames = itemsList.split(",");
+                for (String itemName : itemNames) {
+                    try {
+                        if (initState) {
+                            add(d.getItem(itemName));
+                        }
+                    } catch (Item.NoItemException e) {
+                        throw new Dungeon.IllegalDungeonFormatException(
+                            "No such item '" + itemName + "'");
+                    }
+                }
+            } else {
+                desc += lineOfDesc + "\n";
+            }
+            lineOfDesc = s.nextLine();
+        }
+
+        // throw away delimiter
+        if (!lineOfDesc.equals(Dungeon.SECOND_LEVEL_DELIM)) {
+            throw new Dungeon.IllegalDungeonFormatException("No '" +
+                Dungeon.SECOND_LEVEL_DELIM + "' after room.");
+        }
+    }
+
+    // Common object initialization tasks.
+    private void init() {
+        contents = new ArrayList<Item>();
+        exits = new ArrayList<Exit>();
+        beenHere = false;
+    }
+
+    String getTitle() { return title; }
+
+    void setDesc(String desc) { this.desc = desc; }
+
+    /*
+     * Store the current (changeable) state of this room to the writer
+     * passed.
+     */
+    void storeState(PrintWriter w) throws IOException {
+        w.println(title + ":");
+        w.println("beenHere=" + beenHere);
+        if (contents.size() > 0) {
+            w.print(CONTENTS_STARTER);
+            for (int i=0; i<contents.size()-1; i++) {
+                w.print(contents.get(i).getPrimaryName() + ",");
+            }
+            w.println(contents.get(contents.size()-1).getPrimaryName());
+        }
+        w.println(Dungeon.SECOND_LEVEL_DELIM);
+    }
+
+    void restoreState(Scanner s, Dungeon d) throws 
+        GameState.IllegalSaveFormatException {
+
+        String line = s.nextLine();
+        if (!line.startsWith("beenHere")) {
+            throw new GameState.IllegalSaveFormatException("No beenHere.");
+        }
+        beenHere = Boolean.valueOf(line.substring(line.indexOf("=")+1));
+
+        line = s.nextLine();
+        if (line.startsWith(CONTENTS_STARTER)) {
+            String itemsList = line.substring(CONTENTS_STARTER.length());
+            String[] itemNames = itemsList.split(",");
+            for (String itemName : itemNames) {
+                try {
+                    add(d.getItem(itemName));
+                } catch (Item.NoItemException e) {
+                    throw new GameState.IllegalSaveFormatException(
+                        "No such item '" + itemName + "'");
+                }
+            }
+            s.nextLine();  // Consume "---".
+        }
+    }
+
+    public String describe() {
+        String description;
+        if (beenHere) {
+            description = title;
+        } else {
+            description = title + "\n" + desc;
+        }
+        for (Item item : contents) {
+            description += "\nThere is a " + item.getPrimaryName() + " here.";
+        }
+        if (contents.size() > 0) { description += "\n"; }
+        if (!beenHere) {
+            for (Exit exit : exits) {
+                description += "\n" + exit.describe();
+            }
+        }
+        beenHere = true;
+        return description;
+    }
+    
+    public Room leaveBy(String dir) {
+        for (Exit exit : exits) {
+            if (exit.getDir().equals(dir)) {
+                return exit.getDest();
+            }
+        }
+        return null;
+    }
+
+    void addExit(Exit exit) {
+        exits.add(exit);
+    }
+
+    void add(Item item) {
+        contents.add(item);
+    }
+
+    void remove(Item item) {
+        contents.remove(item);
+    }
+
+    Item getItemNamed(String name) throws Item.NoItemException {
+        for (Item item : contents) {
+            if (item.goesBy(name)) {
+                return item;
+            }
+        }
+        throw new Item.NoItemException();
+    }
+
+    ArrayList<Item> getContents() {
+        return contents;
+    }
+    
+    /**
+    *A method that sets the boolean variable isDark to either true or false,
+    *if true the room requires a lightsource to view its contents,
+    *if false the room may be viewed normally
+    *@author Daniel Zamojda
+    *@return void
+    */
+    public void setIsDark()
+    {}
+    
+    /**
+    *Method that will return the NPC's that are in the room from the NPC arraylist
+    *@return NPC's in the room
+    *@author Daniel Zamojda
+    */
+    public NPC getNPCs()
+    {}
+    
+    /**
+    *Method that will add the NPC's to the NPC arraylist
+    *@param An NPC will be added to the room
+    *@return void
+    *@author Daniel Zamojda
+    */
+    public void addNPCs(NPC npc)
+    {}
+    
+    
+}
